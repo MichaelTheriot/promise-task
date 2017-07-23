caa04243b91086c99c39d9f558ad35cb50d1b173
Michael Theriot <michael.lee.theriot@gmail.com>
2017-07-22 19:38:46 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const store = new WeakMap();
+
+const Task = class extends Promise {
+  constructor(executor) {
+    let resolve, reject;
+    super((subResolve, subReject) => {
+      resolve = subResolve;
+      reject = subReject;
+      if (arguments.length) {
+        executor(resolve, reject);
+      }
+    });
+    store.set(this, {resolve, reject});
+  }
+
+  resolve(value) {
+    store.get(this).resolve(value);
+  }
+
+  reject(value) {
+    store.get(this).reject(value);
+  }
+};
+
+module.exports = Task;
